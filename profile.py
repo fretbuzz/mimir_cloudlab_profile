f75eeef444dbdabcaa9981a36346277f07874c62
Joe <fretbuzz@users.noreply.github.com>
2019-02-14 15:36:39 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -58,7 +58,7 @@ bs.placement = "nonsysvol"
 node.addService(pg.Execute(shell="/bin/sh", command="chmod +x /local/repository/test_profile_script_running.sh"))
 node.addService(pg.Execute(shell="/bin/sh", command="touch /local/repository/test_test.xt"))
 node.addService(pg.Execute(shell="/bin/sh", command="/local/repository/kubernetes_setup.sh"))
-node.addService(pg.Execute(shell="/bin/sh", command="/local/repository/test_profile_script_running.sh"))
+node.addService(pg.Execute(shell="bash", command="/local/repository/test_profile_script_running.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
