d4e77a1d8ce9f9cbeb4d85996824580174e31656
Joe <fretbuzz@users.noreply.github.com>
2019-02-14 14:25:55 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -45,6 +45,7 @@ bs = node.Blockstore("bs", "/mydata")
 bs.size = "80GB"
 bs.placement = "nonsysvol"
 
+node.addService(pg.Execute(shell="bash", command="touch /local/repository/test_test.xt"))
 node.addService(pg.Execute(shell="bash", command="/local/repository/kubernetes_setup.sh"))
 node.addService(pg.Execute(shell="bash", command="/local/repository/test_profile_script_running.sh"))
 
