19991cd6efe67b5bd1c4e3f26b8fd2ebedddeb85
Joe <fretbuzz@users.noreply.github.com>
2019-02-14 16:48:30 -0500

    Update deploy_application.sh

--- deploy_application.sh
+++ deploy_application.sh
@@ -3,15 +3,16 @@ echo $1
 
 if [ "$1" = "wordpress" ]; then
   echo "it was testtest"
+  echo "see, it was wordpress" >> /local/repository/deploy_test.txt
   # deploy_wordpress.py
 elif [ "$1" = "eShop" ]; then
-  echo "hello"
+  echo "see, it was eShop" >> /local/repository/deploy_test.txt
  # deploy_eshop.sh
 elif [ "$1" = "gitlab" ]; then
-  echo "hello gitlab"
+  echo "see, it was gitlab" >> /local/repository/deploy_test.txt
  # deploy_gitlab.py
 elif [ "$1" = "sockshop" ]; then
-  echo "hello shockshop"
+  echo "see, it was sockshop" >> /local/repository/deploy_test.txt
 elif [ "$1" = "drupal" ]; then
-  echo "hello drupal"
+  echo "see, it was drupal" >> /local/repository/deploy_test.txt
 fi
