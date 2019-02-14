59ec71dcf6be3f24dfb383e1e66b08f37f36f9b3
Joe <fretbuzz@users.noreply.github.com>
2019-02-14 16:20:09 -0500

    Create deploy_application.sh

---
+++ deploy_application.sh
@@ -0,0 +1,15 @@
+echo 'this is a test!'
+echo $1
+
+if [ "$1" = "wordpress" ]; then
+  echo "it was testtest"
+  # deploy_wordpress.py
+elif [ "$1" = "eShop" ]; then
+  echo "hello"
+ # deploy_eshop.sh
+elif [ "$1" = "gitlab" ]; then
+  echo "hello gitlab"
+ # deploy_gitlab.py
+elif [ "$1" = "sockshop" ]; then
+  echo "hello shockshop"
+fi
