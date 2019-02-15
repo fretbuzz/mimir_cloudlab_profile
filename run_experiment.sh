404fc3b3a94149870aa7bc449c2b9a796868958d
Joe <fretbuzz@users.noreply.github.com>
2019-02-14 21:01:33 -0500

    Create run_experiment.sh

---
+++ run_experiment.sh
@@ -0,0 +1,11 @@
+kubernetes_setup.sh
+
+deploy_application.sh $1
+
+: ' TODO
+EXP_NAME = None # TODO
+CONFIG_FILE = None # TODO
+PORT_NUMBER = None # TODO
+VM_IP = Nonee # TODO
+/mydata/mimir_snakemake_t2/experiment_coordinator/run_experiment.py --exp_name EXP_NAME --config_file CONFIG_FILE --prepare_app_p --port PORT_NUMBER -ip VM_IP --no_exfil
+'
