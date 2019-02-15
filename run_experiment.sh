12822e4173efef5e86a95eb98279a7996daa116b
Joe <fretbuzz@users.noreply.github.com>
2019-02-14 21:03:58 -0500

    Update run_experiment.sh

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,6 +1,12 @@
+echo 'start run_experiment' >> /local/repository/run_experiment_note1.txt
+
 bash kubernetes_setup.sh
 
+echo 'start run_experiment n2' >> /local/repository/run_experiment_note2.txt
+
 bash deploy_application.sh $1
+
+echo 'start run_experiment n3' >> /local/repository/run_experiment_note3.txt
 
 : ' TODO
 EXP_NAME = None # TODO
@@ -9,3 +15,4 @@ PORT_NUMBER = None # TODO
 VM_IP = Nonee # TODO
 /mydata/mimir_snakemake_t2/experiment_coordinator/run_experiment.py --exp_name EXP_NAME --config_file CONFIG_FILE --prepare_app_p --port PORT_NUMBER -ip VM_IP --no_exfil
 '
+echo 'start run_experiment n4' >> /local/repository/run_experiment_note4.txt
