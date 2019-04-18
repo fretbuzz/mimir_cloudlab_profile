29ca69ef293cd34fa9e672fa195d56067930b581
Joe <fretbuzz@users.noreply.github.com>
2019-04-18 11:03:58 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,7 +1,4 @@
-"""This profile demonstrates a *parameterized profile* that allows you to select one of several standard OS images for your nodes. When you instantiate this profile, you will be asked to select the OS via a drop down menu.
-
-Instructions:
-Click on any node in the topology and choose the `shell` menu item."""
+""" This profile is used to help setup the MIMIR project experimental apparatus. This profile helps setup a Minikube cluster and generate data from microservice applications. Recommended to be used with the run_exp_on_cloudlab.py script from the MIMIR repo."""
 
 # Import the Portal object.
 import geni.portal as portal
@@ -21,60 +18,34 @@ imageList = [
     ('urn:publicid:IDN+wisc.cloudlab.us+image+dna-PG0:minikube_starter:2', 'UBUNTU 16.04'),]
     
 # Define a single parameter for the instantiation page.
-pc.defineParameter("osImage", "Select OS image",
-                   portal.ParameterType.IMAGE,
-                   imageList[0], imageList,
-                   longDescription="Most clusters have this set of images, " +
-                   "pick your favorite one.")
+#pc.defineParameter("osImage", "Select OS image",
+#                   portal.ParameterType.IMAGE,
+#                   imageList[0], imageList,
+#                   longDescription="Most clusters have this set of images, " +
+#                   "pick your favorite one.")
 
-applicationList = [
-    ('none', 'none'), ('wordpress', 'wordpress'), ('sockshop','sockshop'), ('drupal','drupal'), ('eShop','eShop'), ('gitlab', 'gitlab'),]
+#applicationList = [
+#    ('none', 'none'), ('wordpress', 'wordpress'), ('sockshop','sockshop'), ('drupal','drupal'), ('eShop','eShop'), ('gitlab', 'gitlab'),]
 
-#pc.defineParameter( "Application To Start", "Select App", portal.ParameterType.STRING, applicationList[0],
-#                  applicationList, longDescription="Which app to auto start?")
-#portal.context.defineParameter( "n", "Number of VMs", portal.ParameterType.INTEGER, 1 )
-#imageList = [
-#    ('urn:publicid:IDN+wisc.cloudlab.us+image+dna-PG0:minikube_starter:2', 'UBUNTU 16.04'),]
-    
+
 # Define a single parameter for the instantiation page.
-pc.defineParameter("App", "Select App image",
-                   portal.ParameterType.IMAGE,
-                   applicationList[0], applicationList,
-                   longDescription="Most clusters have this set of images, " +
-                   "pick your favorite one.")
+#pc.defineParameter("App", "Select App image",
+#                   portal.ParameterType.IMAGE,
+#                   applicationList[0], applicationList,
+#                   longDescription="Most clusters have this set of images, " +
+#                   "pick your favorite one.")
 
 # Retrieve the values the user specifies during instantiation.
 params = pc.bindParameters()
 
 # Add a raw PC to the request and set the disk image.
 node = request.RawPC("node")
-node.disk_image = params.osImage
+node.disk_image = imageList[0] #params.osImage
 
 # added an ephemeral blockstore per documentation instructions
 bs = node.Blockstore("bs", "/mydata")
 bs.size = "80GB"
 bs.placement = "nonsysvol"
-
-#node.addService(pg.Execute(shell="bash", command="touch /local/repository/test_test.xt"))
-#node.addService(pg.Execute(shell="bash", command="bash /local/repository/test_profile_script_running.sh"))
-#node.addService(pg.Execute(shell="bash", command="chmod +x /local/repository/test_profile_script_running.sh"))
-#node.addService(pg.Execute(shell="bash", command="/local/repository/test_profile_script_running.sh"))
-#node.addService(pg.Execute(shell="bash", command="bash /local/repository/test_profile_script_running.sh"))
-
-#node.addService(pg.Execute(shell="bash", command="chmod +x /local/repository/kubernetes_setup.sh"))
-#node.addService(pg.Execute(shell="bash", command="/local/repository/kubernetes_setup.sh"))
-#node.addService(pg.Execute(shell="bash", command="bash /local/repository/kubernetes_setup.sh"))
-
-#node.addService(pg.Execute(shell="bash", command="chmod +x /local/repository/deploy_application.sh"))
-#node.addService(pg.Execute(shell="bash", command="/local/repository/deploy_application.sh " + str(params.App)))
-#node.addService(pg.Execute(shell="bash", command="bash /local/repository/deploy_application.sh " + str(params.App)))
-
-#run_experiment.sh
-#node.addService(pg.Execute(shell="bash", command="chmod +x /local/repository/run_experiment.sh"))
-#node.addService(pg.Execute(shell="bash", command="/local/repository/run_experiment.sh " + str(params.App)))
-#node.addService(pg.Execute(shell="bash", command="bash /local/repository/run_experiment.sh " + str(params.App)))
-
-
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
